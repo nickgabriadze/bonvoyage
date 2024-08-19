bba2b13f7d467b6404c1804ce5a383a5eb58b709
Nick <nickgabriadze@gmail.com>
2024-08-19 16:34:53 +0400

    refactor: removed unnecessary useEffect, because component still re-renders and updates the state.

--- front-end/src/pages/transfer/components/Dropdown/components/DropdownPlaces.tsx
+++ front-end/src/pages/transfer/components/Dropdown/components/DropdownPlaces.tsx
@@ -3,17 +3,12 @@ import dropdownStyles from '../../styles/dropdown.module.css'
 import {v4} from "uuid";
 import {useAppDispatch, useAppSelector} from "../../../../../states/hooks.ts";
 import {setSelectedLocations} from "../../../../../states/features/dropdownSlice.ts";
-import {useEffect, useState} from "react";
 
 export default function DropdownPlaces() {
     const {selectedLocations} = useAppSelector(s => s.dropdown)
     const dispatch = useAppDispatch()
-    const [locations, setLocations] = useState(places.filter((place: string) => !(place in selectedLocations)))
+   const locations= places.filter((place: string) => !selectedLocations.includes(place))
 
-    useEffect(() => {
-        setLocations(locations.filter((place: string) => !selectedLocations.includes(place)))
-
-    }, [selectedLocations.length])
 
     return <div className={dropdownStyles['placesDropdown']}>
         {locations.map((place) => <div key={v4()}
