eb888bf971518c08fdd079f6fe2b4aaff28ca29b
Nick <nickgabriadze@gmail.com>
2024-08-19 13:38:47 +0400

    refactor: update header navigation

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,18 @@
 import Home from "./pages/home/Home.tsx";
+import {Route, Routes} from "react-router-dom";
+import Header from "./pages/home/components/Header/Header.tsx";
+import Transfers from "./pages/transfer/Transfers.tsx";
 
-export function App(){
+export function App() {
 
 
-    return <Home />
+    return <>
+        <Header/>
+        <Routes>
+            <Route path={'/'} element={<Home/>}/>
+            <Route path={'/transfers'} element={<Transfers />} />
+        </Routes></>
+
 }
 
 export default App;
--- front-end/src/pages/home/components/Header/Header.tsx
+++ front-end/src/pages/home/components/Header/Header.tsx
@@ -2,16 +2,15 @@ import headerStyles from '../styles/header.module.css'
 import {Link} from "react-router-dom";
 
 
-export default function Header(){
+export default function Header() {
+    const URLS = ['/tours', '/transfers', '/contact', '/about']
+
 
     return <header className={headerStyles['homepageHeader']}>
         <h1 className={headerStyles['websiteName']}>Bon Voyage</h1>
 
         <nav className={headerStyles['navigation']}>
-            <Link to={'/tours'}>Tours</Link>
-            <Link to={'/transfers'}>Transfers</Link>
-            <Link to={'/contact'}>Contact</Link>
-            <Link to={'/about'}>About Us</Link>
+            {URLS.map((url, i) => <Link key={i} to={url}>{url.slice(1,)[0].toUpperCase().concat(url.slice(2,))}</Link>)}
         </nav>
     </header>
 }
