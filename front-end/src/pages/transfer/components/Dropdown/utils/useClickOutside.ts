c2e7ec0490e3b31cbe0948b598f9094392505251
Nick <nickgabriadze@gmail.com>
2024-08-20 20:52:34 +0400

    feat: added custom hook to track outside clicks for dropdown menu

---
+++ front-end/src/pages/transfer/components/Dropdown/utils/useClickOutside.ts
@@ -0,0 +1,27 @@
+import {useEffect, useRef} from "react";
+import {useAppSelector} from "../../../../../states/hooks.ts";
+
+export default function useClickOutside(inputID: string, handler: () => void) {
+    const ref = useRef<HTMLDivElement>(null)
+    const {openForInput} = useAppSelector(s => s.dropdown)
+
+    useEffect(() => {
+        const handleClickOutside = (event: any) => {
+            if (openForInput === inputID && !ref.current?.contains(event.target)) {
+                {
+                    handler()
+                }
+            }
+        }
+
+
+        document.addEventListener('mousedown', handleClickOutside)
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+
+    }, [ref.current])
+
+    return ref
+}
