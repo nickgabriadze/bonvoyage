6d7201475a151d914e03c7813dcd99dfe02b25e5
Nick <nickgabriadze@gmail.com>
2024-08-22 16:18:43 +0400

    feat: added link to signup page

--- front-end/src/pages/home/components/Header/Header.tsx
+++ front-end/src/pages/home/components/Header/Header.tsx
@@ -13,19 +13,24 @@ export default function Header() {
         <Link to={'/'}
               className={headerStyles['websiteName']}><h1
             style={loc.pathname !== '/' ? {color: 'var(--color-main-deep-sea-navy-light-5'} : {}}>Bon Voyage</h1></Link>
+
         <nav className={headerStyles['navigation']}>
             {URLS.map((url, i) => <Link key={i} to={url}
                                         style={loc.pathname === url ? {color: 'var(--color-main-deep-sea-navy)',} : {}}
             >{url.slice(1,)[0].toUpperCase().concat(url.slice(2,))}</Link>)}
         </nav>
 
-        <Link to={'/login'} className={headerStyles['userLogin']}
-        onMouseOver={() => setHoverOverLogin(true)}
-             onMouseOut={() => setHoverOverLogin(false)}
+        <div className={headerStyles['userLogin']}
+              onMouseOver={() => setHoverOverLogin(true)}
+              onMouseOut={() => setHoverOverLogin(false)}
         >
-            <img src={hoverOverLogin ? UserFilledSVG: UserSVG} width={24} alt={'User icon'}/>
-            <button>LOGIN</button>
-        </Link>
+            <img src={hoverOverLogin ? UserFilledSVG : UserSVG} width={24} alt={'User icon'}/>
+            <div className={headerStyles['authButtons']}>
+                <Link to={'/login'}><button>LOGIN</button></Link>
+                <span>/</span>
+                <Link to={'/signup'}><button>SIGNUP</button></Link>
+            </div>
+        </div>
     </header>
 }
 
