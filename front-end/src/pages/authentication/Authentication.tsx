e83c83ebe06c89c7393cd8d4f4949508f5fcb756
Nick <nickgabriadze@gmail.com>
2024-08-22 16:18:57 +0400

    feat: created login page

---
+++ front-end/src/pages/authentication/Authentication.tsx
@@ -0,0 +1,7 @@
+import Login from "./login/Login.tsx";
+
+export default function Authentication() {
+
+
+    return <Login />
+}
--- front-end/src/pages/authentication/login/Login.tsx
+++ front-end/src/pages/authentication/login/Login.tsx
@@ -1,5 +1,45 @@
-export default function Login(){
+import loginStyles from './../styles/login.module.css'
+import EmailSVG from '../../../assets/icons/authentication/email.svg'
+import EmailFilledSVG from '../../../assets/icons/authentication/email-filled.svg'
+import PasswordLockSVG from '../../../assets/icons/authentication/password-lock.svg'
+import PasswordLockFilledSVG from '../../../assets/icons/authentication/password-lock-filled.svg'
+import {useState} from "react";
 
 
-    return 'LOGIN'
+export default function Login() {
+    const [focused, setFocused] = useState<string>('')
+
+
+    return <form className={loginStyles['loginFormWrapper']}>
+        <h1>Welcome Back!</h1>
+
+        <div className={loginStyles['inputWrapper']}>
+            <div className={loginStyles['inputWithIcon']}
+                 style={focused === 'email' ? {'outline': '1px solid var(--color-main-deep-sea-navy) '} : {}}
+            >
+                <img src={focused === 'email' ? EmailFilledSVG : EmailSVG} width={24} alt={'Email icon'}/>
+
+                <input type='email' name='email' required={true}
+                       placeholder={'Email'}
+                       onFocus={() => setFocused('email')}
+                       onBlur={() => setFocused('')}
+                />
+            </div>
+            <div className={loginStyles['inputWithIcon']}
+                 style={focused === 'password' ? {'outline': '1px solid var(--color-main-deep-sea-navy) '} : {}}
+
+            >
+                <img src={focused === 'password' ? PasswordLockFilledSVG : PasswordLockSVG} width={24}
+                     alt={'Password lock icon'}/>
+
+                <input type='password' name='password' autoComplete={'true'}
+                       placeholder={'Password'}
+                       onFocus={() => setFocused('password')}
+                       onBlur={() => setFocused('')}
+                       required={true}/>
+            </div>
+        </div>
+
+        <button>Start Exploring</button>
+    </form>
 }
