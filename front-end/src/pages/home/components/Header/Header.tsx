4f56efe3bad22aba5ef53be948b513f18bd57116
Nick <nickgabriadze@gmail.com>
2024-08-22 14:58:16 +0400

    feat: added login button to header that links to /login route

--- front-end/src/pages/home/components/Header/Header.tsx
+++ front-end/src/pages/home/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 import headerStyles from '../styles/header.module.css'
 import {Link, useLocation} from "react-router-dom";
-
+import UserSVG from '../../../../assets/icons/header/user.svg'
+import UserFilledSVG from '../../../../assets/icons/header/user-filled.svg'
+import {useState} from "react";
 
 export default function Header() {
     const URLS = ['/tours', '/transfers', '/contact', '/about']
     const loc = useLocation()
+    const [hoverOverLogin, setHoverOverLogin] = useState<boolean>(false)
 
     return <header className={headerStyles['homepageHeader']}>
         <Link to={'/'}
@@ -15,6 +18,14 @@ export default function Header() {
                                         style={loc.pathname === url ? {color: 'var(--color-main-deep-sea-navy)',} : {}}
             >{url.slice(1,)[0].toUpperCase().concat(url.slice(2,))}</Link>)}
         </nav>
+
+        <Link to={'/login'} className={headerStyles['userLogin']}
+        onMouseOver={() => setHoverOverLogin(true)}
+             onMouseOut={() => setHoverOverLogin(false)}
+        >
+            <img src={hoverOverLogin ? UserFilledSVG: UserSVG} width={24} alt={'User icon'}/>
+            <button>LOGIN</button>
+        </Link>
     </header>
 }
 
