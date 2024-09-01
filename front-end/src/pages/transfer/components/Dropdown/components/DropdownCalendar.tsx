d29c6b7d2b00320edaa6374bfc753f6f68507418
Nick <nickgabriadze@gmail.com>
2024-09-01 21:50:45 +0400

    feat: if dropdown input is open, calendar closes. if date is chosen, 'Choose Date' is replaced with the specified date.

--- front-end/src/pages/transfer/components/Dropdown/components/DropdownCalendar.tsx
+++ front-end/src/pages/transfer/components/Dropdown/components/DropdownCalendar.tsx
@@ -1,17 +1,25 @@
 import Calendar from "../../../../../components/Calendar.tsx";
 import dropdownCalendarStyles from './../../styles/dropdown.calendar.module.css'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import CalendarSVG from './../../../../../assets/icons/calendar/calendar.svg'
 import CalendarFilledSVG from './../../../../../assets/icons/calendar/calendar-filled.svg'
+import {useAppSelector} from "../../../../../states/hooks.ts";
 
 
 export default function DropdownCalendar() {
+    const {openForInput, scheduled} = useAppSelector(s => s.dropdown)
     const [open, setOpen] = useState(false)
+    useEffect(() => {
+        if (openForInput.id !== undefined) {
+            setOpen(false)
+        }
+    }, [openForInput.id])
+
 
     return <section className={dropdownCalendarStyles['calendarWrapper']}>
         <button onClick={() => setOpen((prev) => !prev)} className={dropdownCalendarStyles['datePicker']}>
             <img src={open ? CalendarSVG : CalendarFilledSVG} alt={'Calendar icon'}/>
-            <h4>Choose date</h4>
+            <h4>{scheduled !== undefined ? `${scheduled.day}/${scheduled.month}/${scheduled.year}`:'Choose date'}</h4>
         </button>
         {open && <div className={dropdownCalendarStyles['calendar']}><Calendar/></div>}
     </section>
