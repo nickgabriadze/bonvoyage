bd38327b8f90e59c19c68da60f433390ba27ba2b
Nick <nickgabriadze@gmail.com>
2024-09-07 18:14:35 +0400

    feat: add background to homepage

--- front-end/src/pages/home/components/Home/Home.tsx
+++ front-end/src/pages/home/components/Home/Home.tsx
@@ -1,14 +1,19 @@
 import homeStyles from '../styles/home.module.css'
 import {HowTo} from "./components/HowTo.tsx";
 import {WhatToExpect} from "./components/WhatToExpect.tsx";
+import MountainViewJPEG from '/icons/homepage/mountain-scene.jpg'
 
 export function Home() {
 
     document.title = 'Home'
     return <section className={homeStyles['homeWrapper']}>
         <div className={homeStyles['welcome']}>
+            <img src={MountainViewJPEG} alt={'Mountains view'}/>
             <h1>Explore the wonders of Georgia guided by our professional drivers.</h1>
         </div>
+
+
+
         <HowTo/>
         <WhatToExpect/>
     </section>
