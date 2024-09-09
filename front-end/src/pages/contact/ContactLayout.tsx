a85bcc6a3e3489b44923dcf86aa7e0238100c4ab
Nick <nickgabriadze@gmail.com>
2024-09-10 02:35:03 +0400

    style: add css module class to layout section

--- front-end/src/pages/contact/ContactLayout.tsx
+++ front-end/src/pages/contact/ContactLayout.tsx
@@ -1,16 +1,9 @@
 import {ReactNode} from "react";
+import layoutStyles from './styles/contact.layout.module.css'
 
 export default function ContactLayout({children}: { children: ReactNode }) {
 
-    const layoutStyle = {
-        display: "flex",
-        gap: '60px',
-        alignItems: "center",
-        justifyContent: 'space-around',
-        padding: '10px 10px 10px 10px',
-        borderRadius: '8px',
 
-    }
     return <section
-        style={layoutStyle}>{children}</section>
+        className={layoutStyles['layoutWrapper']}>{children}</section>
 }
